b82d32fc69f5f36ad7e1dc01c6eeac8b0cf474bb
jbeemster <joshua.a.beemster@gmail.com>
2023-10-16 16:42:27 +0800

    Prepare for release

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ locals {
   module_name_suffix = var.kafka_source == "azure_event_hubs" ? "" : ":${var.kafka_source}"
 
   module_name    = "transformer-event-hub-vmss${local.module_name_suffix}"
-  module_version = "0.2.0"
+  module_version = "0.2.1"
 
   app_name = "transformer-kafka"
 
